d3e71b66009bad83182e52efdda8c0d9b8f7fbe3
sebastian.treu <sebastian.treu@54a880da-cad4-11de-9a23-210ccc102f2f>
2009-11-06 13:23:38 +0000

    header changes

--- server.h
+++ server.h
@@ -1,5 +1,25 @@
+/*
+ *   Copyright (c) 2009 Sebastián Treu and Virginia Boscaro.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   Authors: 
+ *         Sebastian Treu 
+ *         Virginia Boscaro
+ *
+ */ 
 #include <openssl/ssl.h>
 
+
+/* CHANGE THIS!!! */
+/* TODO: More work on this header */
 const char *LINE = "\n-----------------------------------------------------------------\n";
 const char *NFO_WAIT  = "[NFO]:\tWaiting for incoming connections\n";
 const char *NFO_HANDLE= "[NFO]:\tTrying to handle new connection on socket: %d\n";
