82597857c48663935883bccdc67988fbc5daca4e
sebastian.treu <sebastian.treu@54a880da-cad4-11de-9a23-210ccc102f2f>
2009-12-21 02:18:26 +0000

    not done

--- trunk/src/list.c
+++ trunk/src/list.c
@@ -1,4 +1,23 @@
+/*
+ *   Copyright (c) 2009 Sebastián Treu and Virginia Boscaro.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   Authors: 
+ *         Sebastian Treu 
+ *         Virginia Boscaro
+ *
+ */
+
 #include <stdlib.h>
+
 #include "list.h"
 
 list_t *list_make_empty(list_t *listPtr)
