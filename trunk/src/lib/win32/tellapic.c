bd6e40bd1ac1be71bce0042565339a41b4e1298a
sebastian.treu@gmail.com <sebastian.treu@gmail.com@54a880da-cad4-11de-9a23-210ccc102f2f>
2011-05-09 19:03:54 +0000

    New macro definitions added to windows tellapic.c file.

--- trunk/src/lib/win32/tellapic.c
+++ trunk/src/lib/win32/tellapic.c
@@ -1,6 +1,13 @@
-#include <winsock.h>
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
 #include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 #pragma comment(lib, "Ws2_32.lib")
+
 #include "tellapic/tellapic.h"
 
 
