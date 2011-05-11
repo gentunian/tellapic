17a1c318d6d1c4d3163525e74d96be82b9f68a7d
sebastian.treu@gmail.com <sebastian.treu@gmail.com@54a880da-cad4-11de-9a23-210ccc102f2f>
2011-05-11 09:06:30 +0000

    Read functions poorly implemented for now. Work to do.

--- src/lib/win32/tellapic.c
+++ src/lib/win32/tellapic.c
@@ -45,38 +45,37 @@ _read_nb(tellapic_socket_t socket, size_t totalbytes, byte_t *buf)
 POSH_PUBLIC_API(size_t)
 _read_b(tellapic_socket_t socket, size_t totalbytes, byte_t *buf) 
 {
-	return 1;
-  //size_t bytesleft = totalbytes;
-  //size_t bytesread = 0;
+	
+  size_t bytesleft = totalbytes;
+  size_t bytesread = 0;
 
-  ///* This function should be feeded with a file descriptor with the O_NONBLOCK flag unset. */
-  ///* The *_b means that this function WILL block until data arrives. */
-  ///* Get the fd flags. */
-  //int flags = fcntl(fd, F_GETFL);
+  /* This function should be feeded with a file descriptor with the O_NONBLOCK flag unset. */
+  /* The *_b means that this function WILL block until data arrives. */
+  /* Get the fd flags. */
+  /*int flags = fcntl(fd, F_GETFL);*/
 
-  ///* If fd has O_NONBLOCK set, unset it. THIS IS MANDATORY, that's what _b means. */
-  //if ((flags & O_NONBLOCK) == O_NONBLOCK)
-    //fcntl(fd, F_SETFL, flags & ~O_NONBLOCK);
+  /* If fd has O_NONBLOCK set, unset it. THIS IS MANDATORY, that's what _b means. */
+  /*if ((flags & O_NONBLOCK) == O_NONBLOCK)
+    fcntl(fd, F_SETFL, flags & ~O_NONBLOCK);
+*/
+  /* Now we have a fd in blocking mode... */
+  while(bytesleft > 0) 
+    {
+	  size_t bytes = recv(socket, buf + bytesread, bytesleft, 0);
 
-  ///* Now we have a fd in blocking mode... */
-  //while(bytesleft > 0) 
-    //{
+      if (bytes > 0)
+	{
+	  bytesleft -= bytes;
+	  bytesread += bytes;
+	}
+      else
+	bytesleft = 0;
+    }
 
-      //size_t bytes = read(fd, buf + bytesread, bytesleft);
+  /* Restore the flags */
+ /* fcntl(fd, F_SETFL, flags);*/
 
-      //if (bytes > 0)
-	//{
-	  //bytesleft -= bytes;
-	  //bytesread += bytes;
-	//}
-      //else
-	//bytesleft = 0;
-    //}
-
-  ///* Restore the flags */
-  //fcntl(fd, F_SETFL, flags);
-
-  //return bytesread;
+  return bytesread;
 }
 
 
@@ -84,15 +83,17 @@ _read_b(tellapic_socket_t socket, size_t totalbytes, byte_t *buf)
  *
  */
 POSH_PUBLIC_API(tellapic_socket_t)
-tellapic_connect_to(const char *hostname, int port)
+tellapic_connect_to(const char *hostname, const char *port)
 {
   /* From MSDN API DOC and Resources ... */
   SOCKET ConnectSocket = INVALID_SOCKET;
   WSADATA wsaData;
   int     iResult;
-  struct addrinfo *result = NULL,
-    *ptr = NULL,
-    hints;
+  struct addrinfo *result = NULL;
+  struct addrinfo  *ptr = NULL;
+  struct addrinfo  hints;
+
+  printf("INIT\n");
 
   /* Initialize Winsock */
   iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
@@ -107,6 +108,8 @@ tellapic_connect_to(const char *hostname, int port)
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_protocol = IPPROTO_TCP;
 
+  printf("INIT\n");
+
   /* Resolve the server address and port */
   iResult = getaddrinfo(hostname, port, &hints, &result);
   if (iResult != 0) 
@@ -116,12 +119,16 @@ tellapic_connect_to(const char *hostname, int port)
       return -1;
     }
 
+  printf("INIT\n");
+
   /* Attempt to connect to the first address returned by */
   /* the call to getaddrinfo */
   ptr=result;
 
   /* Create a SOCKET for connecting to server */
   ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+  printf("INIT\n");
 
   if (ConnectSocket == INVALID_SOCKET) 
     {
@@ -139,12 +146,16 @@ tellapic_connect_to(const char *hostname, int port)
       ConnectSocket = INVALID_SOCKET;
     }
 
+  printf("INIT\n");
+
   /* Should really try the next address returned by getaddrinfo */
   /* if the connect call failed */
   /* But for this simple example we just free the resources */
   /* returned by getaddrinfo and print an error message */
 
   freeaddrinfo(result);
+
+  printf("INIT\n");
 
   if (ConnectSocket == INVALID_SOCKET) 
     {
@@ -153,6 +164,7 @@ tellapic_connect_to(const char *hostname, int port)
       return -1;
     }
 
+  printf("Open socket: %d\n", ConnectSocket);
   return ConnectSocket;
 }
 
