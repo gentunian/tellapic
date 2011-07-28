3eb387636dc78b55e7f508c968d9c88e6afa35e6
sebastian.treu@gmail.com <sebastian.treu@gmail.com@54a880da-cad4-11de-9a23-210ccc102f2f>
2011-07-28 15:05:20 +0000

    Adding distutils setup script for building the python extension. Still work to do.

---
+++ trunk/src/swig/python/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+setup module for building SWIG tellapic library extension bindings
+"""
+
+from distutils.core import setup, Extension
+import TellapicConfig
+
+pytellapic_module = Extension("_"+TellapicConfig.EXTENSION_NAME,
+                              sources=[TellapicConfig.SWIG_GENERATED_FILE]+TellapicConfig.SOURCE_FILES,
+
+                              swig_opts=['module '+TellapicConfig.EXTENSION_NAME,'cpperraswarn'+TellapicConfig.OS_DEFINITIONS],
+                              )
+
+setup(name=TellapicConfig.EXTENSION_NAME,
+      version=TellapicConfig.EXTENSION_VERSION,
+      author="Sebastian Treu",
+      description='Tellapic protocol library extension.',
+      ext_modules=[pytellapic_module],
+      py_modules=[TellapicConfig.EXTENSION_NAME],
+      )
+      
